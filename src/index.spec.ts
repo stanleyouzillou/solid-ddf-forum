307b3aec47a0a552081860d29597f89d50d5cba4
Stanley Ouzillou <stanleyouzillou@gmail.com>
2024-07-17 00:15:48 +0200

    [Red]: return 'FizzBuzz" for 15

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -4,10 +4,13 @@ describe("fizzbuzz function", () => {
   it("should return a string", () => {
     expect(fizzBuzz(3)).toEqual(expect.any(String));
   });
-  it("should return fizz when input is 3", () => {
+  it("should return Fizz when input is 3", () => {
     expect(fizzBuzz(3)).toBe("Fizz");
   });
-  it("should return buzz for 5", () => {
+  it("should return Buzz for 5", () => {
     expect(fizzBuzz(5)).toBe("Buzz");
+  });
+  it("should return FizzBuzz for 15", () => {
+    expect(fizzBuzz(15)).toBe("FizzBuzz");
   });
 });
