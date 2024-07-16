1e6fe9b58d772a5e4e5b39556b1fc59b817db8ba
Stanley Ouzillou <stanleyouzillou@gmail.com>
2024-07-17 00:32:25 +0200

    [Refactor]: refactored tests to represent the higher level of functionality of 'returns Fizz' for multiples of 3

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -4,15 +4,20 @@ describe("fizzbuzz function", () => {
   it("should return a string", () => {
     expect(fizzBuzz(3)).toEqual(expect.any(String));
   });
-  it("should return Fizz when input is 3", () => {
-    expect(fizzBuzz(3)).toBe("Fizz");
+
+  describe('returns "Fizz" for multiples of 3', () => {
+    it.each([3, 6, 9])('returns "Fizz for multiples of 3', (value: number) => {
+      expect(fizzBuzz(value)).toBe("Fizz");
+    });
   });
+
   it("should return Buzz for 5", () => {
     expect(fizzBuzz(5)).toBe("Buzz");
   });
   it("should return FizzBuzz for 15", () => {
     expect(fizzBuzz(15)).toBe("FizzBuzz");
   });
+
   it("should return Fizz for 9", () => {
     expect(fizzBuzz(9)).toBe("Fizz");
   });
