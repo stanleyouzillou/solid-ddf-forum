1b3b4cc9c0f951e30c2b9cf60eb6fbb97061ccd7
Stanley Ouzillou <stanleyouzillou@gmail.com>
2024-07-18 03:54:37 +0200

    Requirements and test rig setup

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,5 +1,7 @@
+import { Calculator } from ".";
 
-describe('stats calculator', () => {
-
-
-})
+describe("stats calculator", () => {
+  it("should print -8 as the minimum value for the given sequence of number [2, 4, 21, -8, 53, 40]", () => {
+    expect(Calculator.minimum([2, 4, 21, -8, 53, 40])).toBe(-8);
+  });
+});
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export class Calculator {
+  static minimum(sequenceOfNumber: number[]): number {
+    return -8;
+  }
+}
