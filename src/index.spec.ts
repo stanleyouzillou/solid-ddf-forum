ed4184ead56f83e198b0650ce0b46f8684196fa7
Stanley Ouzillou <stanleyouzillou@gmail.com>
2024-07-19 05:36:56 +0200

    [Reformat] : better readibility for main function and tests

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,15 +1,31 @@
 import { militaryTimeValidator } from "./index";
 
-describe("military time validator", () => {
-  it("should return true if time 1 is before time 2", () => {
+describe("militaryTimeValidator", () => {
+  it('should return true for valid time format "01:12 - 14:32"', () => {
     expect(militaryTimeValidator("01:12 - 14:32")).toBe(true);
   });
 
-  it("should return false if time 1 is after time 2", () => {
-    expect(militaryTimeValidator("15:12 - 14:32")).toBe(false);
+  it('should return false for invalid time format "1:12 - 14:32"', () => {
+    expect(militaryTimeValidator("1:12 - 14:32")).toBe(false);
   });
 
-  it("should return false if time 1 is equal to time 2", () => {
+  it("should validate correct time formats with valid hour and minute ranges", () => {
+    expect(militaryTimeValidator("00:00 - 23:59")).toBe(true);
+    expect(militaryTimeValidator("00:00 - 00:01")).toBe(true);
+  });
+
+  it("should invalidate correct time formats with invalid hour or minute ranges", () => {
+    expect(militaryTimeValidator("25:00 - 14:32")).toBe(false);
+    expect(militaryTimeValidator("14:60 - 14:32")).toBe(false);
+    expect(militaryTimeValidator("12:00 - 24:01")).toBe(false);
+    expect(militaryTimeValidator("12:00 - 24:00")).toBe(false);
+  });
+
+  it("should validate that time 1 is before time 2", () => {
+    expect(militaryTimeValidator("01:12 - 14:32")).toBe(true);
+    expect(militaryTimeValidator("14:32 - 01:12")).toBe(false);
     expect(militaryTimeValidator("14:32 - 14:32")).toBe(false);
+    expect(militaryTimeValidator("23:59 - 00:00")).toBe(false);
+    expect(militaryTimeValidator("23:59 - 23:59")).toBe(false);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,14 @@ export function militaryTimeValidator(timeRange: string): boolean {
 
   if (match) {
     const [, XH, XM, YH, YM] = match;
+
     const hourXH = parseInt(XH, 10);
     const minuteXM = parseInt(XM, 10);
     const hourYH = parseInt(YH, 10);
     const minuteYM = parseInt(YM, 10);
 
     const isValidHour =
-      hourXH >= 0 && hourXH <= 24 && hourYH >= 0 && hourYH <= 24;
+      hourXH >= 0 && hourXH <= 23 && hourYH >= 0 && hourYH <= 23;
     const isValidMinute =
       minuteXM >= 0 && minuteXM <= 59 && minuteYM >= 0 && minuteYM <= 59;
 
